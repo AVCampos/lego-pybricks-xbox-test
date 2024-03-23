8c293ed3f8dd12a9acf320d5a97a656696773703
Alex Campos <91068810+AVCampos@users.noreply.github.com>
2024-03-23 01:12:42 +0000

    Added comments and cleanup

--- xbox_test.py
+++ xbox_test.py
@@ -1,27 +1,34 @@
 from pybricks.hubs import TechnicHub
-from pybricks.parameters import Color
+from pybricks.parameters import Button, Color
 from pybricks.tools import wait
 from pybricks.iodevices import XboxController
 
+# Initialisation.
 hub = TechnicHub()
 controller = XboxController()
+hub.light.off() # The controller finished connecting, so we signal that.
 
+# Returns true if a button is pressed or a stick/trigger is moved from its neutral position.
 def inputPressed():
     return controller.buttons.pressed() != set() or controller.dpad() != 0 or controller.joystick_left() != (0, 0) or controller.joystick_right() != (0, 0) or controller.triggers() != (0, 0)
 
 while(True):
+    # Turn off the LED if there are no inputs.
     if(not inputPressed()):
         hub.light.off()
 
+    # Wait for an input.
     while(not inputPressed()):
         pass
     
+    # Read the inputs, to be processed later.
     dpad = controller.dpad()
     buttonsPressed = controller.buttons.pressed()
     stick1 = controller.joystick_left()
     stick2 = controller.joystick_right()
     triggers = controller.triggers()
 
+    # Turn the LED on in different colours depending on the input.
     if(dpad != 0):
         hub.light.on(Color.ORANGE)
     elif(buttonsPressed != set()):
@@ -53,5 +60,5 @@ while(True):
             intensity = abs(triggers[1]) / 100
             print(intensity)
             hub.light.on(Color.RED * intensity)
-            
-    wait(10)
+
+    wait(10) # Calm down a bit before polling the inputs again.
